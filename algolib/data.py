a47ebd68b80bba767542051bb393306fc3950b8e
Dan Pitic <dpitic@gmail.com>
2020-09-04 20:50:47 +0800

    Refactor default Google data file name.
    The macOS filesystem is not case sensitive and two files with the same name
    but with different case caused a clash.

--- algolib/data.py
+++ algolib/data.py
@@ -5,14 +5,14 @@ import pandas_datareader as pdr
 from statsmodels.tsa.stattools import adfuller
 
 
-def get_google_data(data_file='data/goog_data.pkl', start_date='2014-01-01',
+def get_google_data(data_file='data/GOOG_data.pkl', start_date='2014-01-01',
                     end_date='2018-01-01'):
     """Return the Google financial data from Yahoo Finance.
 
     This function tries to load the specified data file otherwise it downloads
     the Google financial data from Yahoo Finance.
     :param str data_file: Filename with path for the Google finance data,
-        default='data/goog_data.pkl'.
+        default='data/GOOG_data.pkl'.
     :param str start_date: Data start date, default='2014-01-01'.
     :param str end_date: Data end date, default='2018-01-01'.
     :return: DataFrame with Google financial data.
