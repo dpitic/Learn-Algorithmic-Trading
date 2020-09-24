b3f2ecca7fd07376353253cb42ba267651e11366
Dan Pitic <dpitic@gmail.com>
2020-09-24 22:07:08 +0800

    Documented trading strategy and implemented unit tests.

---
+++ ch07/test_trading_strategy.py
@@ -0,0 +1,78 @@
+import unittest
+
+from ch07.trading_strategy import TradingStrategy
+
+
+class TestTradingStrategy(unittest.TestCase):
+    def setUp(self) -> None:
+        self.trading_strategy = TradingStrategy(cash=10000)
+
+    def test_receive_top_of_book(self):
+        # Test event message from the exchange (through the order book)
+        book_event = {
+            'bid_price': 12,
+            'bid_quantity': 100,
+            'offer_price': 11,
+            'offer_quantity': 150
+        }
+        self.trading_strategy.handle_book_event(book_event)
+        self.assertEqual(len(self.trading_strategy.orders), 2)
+        self.assertEqual(self.trading_strategy.orders[0]['price'], 12)
+        self.assertEqual(self.trading_strategy.orders[0]['quantity'], 100)
+        self.assertEqual(self.trading_strategy.orders[0]['side'], 'sell')
+        self.assertEqual(
+            self.trading_strategy.orders[0]['action'], 'no_action')
+        self.assertEqual(self.trading_strategy.orders[1]['price'], 11)
+        self.assertEqual(self.trading_strategy.orders[1]['quantity'], 100)
+        self.assertEqual(self.trading_strategy.orders[1]['side'], 'buy')
+        self.assertEqual(
+            self.trading_strategy.orders[1]['action'], 'no_action')
+
+    def test_rejected_order(self):
+        # Get book event message and handle the event
+        self.test_receive_top_of_book()
+        # Create market response indicating a rejection of order with id=1
+        order_execution = {
+            'id': 1,
+            'price': 12,
+            'quantity': 100,
+            'side': 'sell',
+            'status': 'rejected'
+        }
+        self.trading_strategy.handle_market_response(order_execution)
+        # Ensure the trading strategy removes the rejected order
+        self.assertEqual(self.trading_strategy.orders[0]['price'], 11)
+        self.assertEqual(self.trading_strategy.orders[0]['quantity'], 100)
+        self.assertEqual(self.trading_strategy.orders[0]['side'], 'buy')
+        self.assertEqual(self.trading_strategy.orders[0]['status'], 'new')
+
+    def test_filled_order(self):
+        # Get book event message and handle the event
+        self.test_receive_top_of_book()
+        # Create market response indicating filled order for order id=1
+        order_execution = {
+            'id': 1,
+            'price': 12,
+            'quantity': 100,
+            'side': 'sell',
+            'status': 'filled'
+        }
+        self.trading_strategy.handle_market_response(order_execution)
+        self.assertEqual(len(self.trading_strategy.orders), 1)
+
+        # Create market response indicating filled order for order id=2
+        order_execution = {
+            'id': 2,
+            'price': 11,
+            'quantity': 100,
+            'side': 'buy',
+            'status': 'filled'
+        }
+        self.trading_strategy.handle_market_response(order_execution)
+        self.assertEqual(self.trading_strategy.position, 0)
+        self.assertEqual(self.trading_strategy.cash, 10100)
+        self.assertEqual(self.trading_strategy.pnl, 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- ch07/trading_strategy.py
+++ ch07/trading_strategy.py
@@ -13,11 +13,18 @@ class TradingStrategy:
                     managing the order lifecycle.
     """
 
-    def __init__(self, cash, ob_2_ts, ts_2_om, om_2_ts):
+    def __init__(self, cash, ob_2_ts=None, ts_2_om=None, om_2_ts=None):
+        """Initialise a TradingStrategy object.
+        :param cash: Initial amount of money for this trading strategy.
+        :param ob_2_ts: Order book to trading strategy message channel,
+            default=None places the object in simulation mode.
+        :param ts_2_om: Trading strategy to order manager message channel,
+            default=Nome places the object in simulation mode.
+        """
         self.orders = []
         self.order_id = 0
         self.position = 0
-        self.pnl = 0
+        self._pnl = 0
         self.cash = cash
         self.current_bid = 0
         self.current_offer = 0
@@ -25,32 +32,51 @@ class TradingStrategy:
         self.ts_2_om = ts_2_om
         self.om_2_ts = om_2_ts
 
-    def create_orders(self, book_event, quantity):
-        """Create two orders simultaneously for an arbitrage situation."""
+    def create_order(self, book_event, quantity):
+        """Create two orders simultaneously for an arbitrage situation.
+
+        This method creates a sell order and a buy order for the specified
+        quantity. The sell price is the bid price in the book event, and the buy
+        price is the offer price in the book event. The orders are added to the
+        list of orders this trading strategy object manages.
+        """
         self.order_id += 1
-        order = {
+        sell_order = {
             'id': self.order_id,
             'price': book_event['bid_price'],
             'quantity': quantity,
             'side': 'sell',
             'action': 'to_be_sent'
         }
-        self.orders.append(order.copy())
+        self.orders.append(sell_order.copy())
 
         self.order_id += 1
-        order = {
+        buy_order = {
             'id': self.order_id,
             'price': book_event['offer_price'],
             'quantity': quantity,
             'side': 'buy',
             'action': 'to_be_sent'
         }
-        self.orders.append(order.copy())
+        self.orders.append(buy_order.copy())
 
     def signal(self, book_event):
-        """Return True if the bid price is higher than the ask price."""
-        if book_event is not None:
+        """Return True if the bid price is higher than the ask price.
+
+        This method inspects a book event to determine whether a trading signal
+        has been found. A trading signal occurs if the bid price is less than
+        the offer price in the book event and the message is valid, i.e. both
+        the bid price and the offer price are greater than zero. That way a 
+        profit can be made by buying a the lower bid price and selling at the 
+        higher offer price.
+        :param book_event: Book event message from the exchange.
+        :return: True if the bid price is higher than the ask price, otherwise
+            False.
+        """
+        if book_event is not None:  # message validation
+            # Check if profit can be made (i.e. trading signal)
             if book_event['bid_price'] > book_event['offer_price']:
+                # Validate message from exchange (order book)
                 if book_event['bid_price'] > 0 and \
                         book_event['offer_price'] > 0:
                     return True
@@ -63,10 +89,10 @@ class TradingStrategy:
         """Manage processing orders for the whole order lifecycle. When an order
         is created, its status is 'new'. Once the order has been sent to the
         market, the market will respond by acknowledging the order or reject
-        the order. If the order is rejected, it is removed from the list of
-        outstanding orders.
+        the order. If the order is rejected or filled, it is removed from the 
+        list of outstanding orders.
         """
-        orders_to_be_removed = []
+        orders_to_be_removed = []  # index of rejected orders
         for index, order in enumerate(self.orders):
             if order['action'] == 'to_be_sent':
                 order['status'] = 'new'
@@ -74,21 +100,32 @@ class TradingStrategy:
                 if self.ts_2_om is None:
                     print('Simulation mode')
                 else:
+                    # Send a copy of the order to the order manager
                     self.ts_2_om.append(order.copy())
-            if order['status'] == 'rejected':
+            elif order['status'] == 'rejected':
+                # Add order index to the rejection list to be removed
                 orders_to_be_removed.append(index)
-            if order['status'] == 'filled':
+            elif order['status'] == 'filled':
+                # Order is filled; end of order lifecycle
                 orders_to_be_removed.append(index)
                 position = order['quantity'] if order['side'] == 'buy' \
                     else -order['quantity']
                 self.position += position
-                self.pnl -= position * order['price']
+                self._pnl -= position * order['price']
                 self.cash -= position * order['price']
+        # Remove rejected and filled orders
         for order_index in sorted(orders_to_be_removed, reverse=True):
             del self.orders[order_index]
 
-    def handle_input_from_bb(self, book_event=None):
-        """Check whether there are book events in the deque ob_2_ts."""
+    def handle_input_from_ob(self, book_event=None):
+        """Check whether there are book events in the deque ob_2_ts.
+
+        This method handles input messages from the order book. If the message
+        channel between the order book and the trading strategy is not 
+        configured, a book event message can be passed in for testing purposes.
+        :param book_event: Book event message from the order book, default=None
+            in which case the trading strategy is operating in simulation mode.
+        """
         if self.ob_2_ts is None:
             print('Simulation mode')
             self.handle_book_event(book_event)
@@ -97,24 +134,42 @@ class TradingStrategy:
                 self.handle_book_event(self.ob_2_ts.popleft())
 
     def handle_book_event(self, book_event):
-        """Check whether there is a signal to send an order."""
+        """Check whether there is a signal in the book event to send an order.
+
+        This method implements the logic to detect trading signals from book 
+        event messages obtained from the order book. If a trading signal is
+        found, it creates an order based on the book event for the minimum
+        quantity of bid quantity and offer quantity.
+        :param book_event: Book event message from the order book.
+        """
         if book_event is not None:
             self.current_bid = book_event['bid_price']
             self.current_offer = book_event['offer_price']
 
         # Check whether there is a signal to send an order
         if self.signal(book_event):
-            self.create_orders(book_event, min(book_event['bid_quantity'],
-                                               book_event['offer_quantity']))
+            self.create_order(book_event, min(book_event['bid_quantity'],
+                                              book_event['offer_quantity']))
         self.execution()
 
     def get_order(self, order_id):
-        """Return order and order index."""
-        index = 0
-        for order in self.orders:
+        """Return reference to the order and the order index.
+
+        This method looks up the orders in the trading strategy and returns the
+        order and its index, if found.
+        :param order_id: Order id to look up in the order this trading strategy
+            manages.
+        :return: Tuple of order and index, if the order id is found, otherwise
+            None.
+        """
+        # index = 0
+        # for order in self.orders:
+        #     if order['id'] == order_id:
+        #         return order, index
+        #     index += 1
+        for index, order in enumerate(self.orders):
             if order['id'] == order_id:
                 return order, index
-            index += 1
         return None, None
 
     def handle_response_from_om(self):
@@ -126,15 +181,37 @@ class TradingStrategy:
             print('Simulation mode')
 
     def handle_market_response(self, order_execution):
-        """Process order from order manager."""
-        order, index = self.get_order(order_execution['id'])
-        if order is None:
-            print('Error: order not found')
-            return
-        order['status'] = order_execution['status']
-        self.execution()
+        """Process order from order manager.
 
-    def get_pnl(self):
-        """Return profit and loss."""
-        return self.pnl + \
+        This method implements the logic to handle order execution messages
+        from the market (obtained through the order manager). It looks up the
+        order corresponding to the order execution message order id, and updates
+        its status to align it with the status in the order execution message.
+        It then delegates to the execution module of the trading strategy to
+        manage the order lifecycle. If the order is not found, the message is 
+        dropped.
+        :param order_execution: Order execution message obtained from the order
+            manager.
+        """
+        # Look up order corresponding to the order id in the message
+        order, index = self.get_order(order_execution['id'])
+        # Check if there is a corresponding order
+        if order is None:
+            # Corresponding order not found; drop message
+            print('Error: order not found')
+        else:
+            # Order found; update order status to status of message
+            order['status'] = order_execution['status']
+            # Process orders managed by this trading strategy
+            self.execution()
+
+    @property
+    def pnl(self):
+        """Return profit and loss.
+
+        This method calculates the profit and loss based on the current profit
+        and loss, position and the current bid and current offer.
+        :return: Profit and loss.
+        """
+        return self._pnl + \
                self.position * (self.current_bid + self.current_offer) / 2
